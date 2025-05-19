e4f1ff8b56b56d77494ff6a00331101af2bb1e9f
Methminpulsara <methminpulsara10@gmail.com>
2025-05-19 20:37:29 +0530

    Last commit

--- src/main/java/edu/icet/ecom/controler/ChatMessageController.java
+++ src/main/java/edu/icet/ecom/controler/ChatMessageController.java
@@ -3,32 +3,30 @@ package edu.icet.ecom.controler;
 import edu.icet.ecom.dto.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+
 @RestController
-@RequestMapping("/api")
-@CrossOrigin
+@CrossOrigin("*")
 @RequiredArgsConstructor
 public class ChatMessageController {
 
-    private final SimpMessagingTemplate simpMessagingTemplate;
-
-
-    @MessageMapping("/message") //app/message
+    private final SimpMessagingTemplate messagingTemplate;
+    @MessageMapping("/message")
     @SendTo("/chatroom/public")
-    private ChatMessage receivePublicMessage(ChatMessage message){
+    public ChatMessage receiveMessage(@Payload ChatMessage message){
         return message;
     }
 
     @MessageMapping("/private-message")
-    public ChatMessage receivePrivateMessage(ChatMessage message){
-        simpMessagingTemplate.convertAndSendToUser(message.getRecipient(),"/private", message);
+    public ChatMessage recMessage(@Payload ChatMessage message){
+        messagingTemplate.convertAndSendToUser(message.getRecipient(),"/private",message);
+        System.out.println(message.toString());
         return message;
     }
-
-
-
-
 }
+
